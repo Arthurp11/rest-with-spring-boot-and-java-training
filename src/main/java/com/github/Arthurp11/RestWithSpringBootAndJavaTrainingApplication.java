d2811168be8086369cbbb77c9baaa808993e37b7
arthurprimo <tutuprimo@gmail.com>
2024-11-12 15:42:27 -0300

    first commit

---
+++ src/main/java/com/github/Arthurp11/RestWithSpringBootAndJavaTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.github.Arthurp11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Arthurp11/RestWithSpringBootAndJavaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Arthurp11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
